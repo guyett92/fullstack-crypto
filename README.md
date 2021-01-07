737f9348ca258188fc6348f1f8810220dbfb2a48
Aaron Guyett <aaronguyett@yahoo.com>
2021-01-07 01:00:20 -0800

    configure production code

--- README.md
+++ README.md
@@ -2,6 +2,22 @@
 
 * 
 
+## Get started
+The app is configured to work with PubNub on Heroku. To run locally you can use one of the following options:
+
+### Windows
+
+* The development environment is not configured for Windows. To configure it the following changes must be made:
+* `dev-client` script needs to be `"dev-client": "npm run clean && parcel client/src/index.html --out-dir client/dist",`
+* `dev` script needs to be `"dev": "cross-env ENV='development' nodemon index.js",`
+* This will work if you run each command in a separate terminal instance (one for the server and one for the frontend)
+
+### Linux & Mac
+
+* The current configuration will work for Linux and Mac users. The commands with single ampersands do not work with Windows but if they are changed the scripts should be the following:
+* `dev-client` script needs to be `npm run clean && parcel client/src/index.html --out-dir client/dist`
+* `dev` script needs to be `npm run dev-client && cross-env ENV='development' nodemon index.js`
+
 ## Technologies Used
 
 * Node
--- index.js
+++ index.js
@@ -8,6 +8,8 @@ const TransactionPool = require('./wallet/transaction-pool');
 const Wallet = require('./wallet');
 const TransactionMiner = require('./app/transaction-miner');
 
+const isDevelopment = process.env.ENV === 'development';
+
 const app = express();
 const blockchain = new Blockchain();
 const transactionPool = new TransactionPool();
@@ -102,42 +104,44 @@ const syncWithRootState = () => {
 };
 
 // Generate filler data
-const walletFoo = new Wallet();
-const walletBar = new Wallet();
+if(idDevelopment) {
+    const walletFoo = new Wallet();
+    const walletBar = new Wallet();
 
-const generateWalletTransaction = ({ wallet, recipient, amount }) => {
-    const transaction = wallet.createTransaction({
-        recipient, amount, chain: blockchain.chain
-    });
+    const generateWalletTransaction = ({ wallet, recipient, amount }) => {
+        const transaction = wallet.createTransaction({
+            recipient, amount, chain: blockchain.chain
+        });
 
-    transactionPool.setTransaction(transaction);
-}
-
-const walletAction = () => generateWalletTransaction({
-    wallet, recipient: walletFoo.publicKey, amount: 5
-});
-
-const walletFooAction = () => generateWalletTransaction({
-    wallet: walletFoo, recipient: walletBar.publicKey, amount: 10
-});
-
-const walletBarAction = () => generateWalletTransaction({
-    wallet: walletBar, recipient: wallet.publicKey, amount: 15
-});
-
-for(let i = 0; i < 10; i++) {
-    if(i % 3 === 0) {
-        walletAction();
-        walletFooAction();
-    } else if(i % 3 === 1) {
-        walletAction();
-        walletBarAction();
-    } else {
-        walletFooAction();
-        walletBarAction();
+        transactionPool.setTransaction(transaction);
     }
 
-    transactionMiner.mineTransactions();
+    const walletAction = () => generateWalletTransaction({
+        wallet, recipient: walletFoo.publicKey, amount: 5
+    });
+
+    const walletFooAction = () => generateWalletTransaction({
+        wallet: walletFoo, recipient: walletBar.publicKey, amount: 10
+    });
+
+    const walletBarAction = () => generateWalletTransaction({
+        wallet: walletBar, recipient: wallet.publicKey, amount: 15
+    });
+
+    for(let i = 0; i < 10; i++) {
+        if(i % 3 === 0) {
+            walletAction();
+            walletFooAction();
+        } else if(i % 3 === 1) {
+            walletAction();
+            walletBarAction();
+        } else {
+            walletFooAction();
+            walletBarAction();
+        }
+
+        transactionMiner.mineTransactions();
+    }
 }
 
 // End filler data
@@ -148,7 +152,7 @@ if(process.env.GENERATE_PEER_PORT === 'true') {
     PEER_PORT = DEFAULT_PORT + Math.ceil(Math.random() * 1000);
 }
 
-const PORT = PEER_PORT || DEFAULT_PORT;
+const PORT = process.env.PORT || PEER_PORT || DEFAULT_PORT;
 app.listen(PORT, () => {
     console.log(`listening at localhost:${PORT}`);
 
